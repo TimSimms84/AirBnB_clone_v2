ef4e78871ff767ae15eadbbce2a6a29d6965101c
Tim Simms <tsimms@gmail.com>
2022-03-16 16:58:07 -0500

    pep8 issues

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,14 +10,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route("/", strict_slashes=False)
 def hello():
-    """displays 'Hello HBNB!"""
+    """displays 'Hello HBNB!'"""
     return "Hello HBNB!"
-
-
-
 
 
 if __name__ == "__main__":
