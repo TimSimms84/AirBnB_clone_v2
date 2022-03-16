f7f138ebbac3909c6d7df483752477e5e43ff3f2
Tim Simms <tsimms@gmail.com>
2022-03-16 16:55:07 -0500

    add init file and script that servers HELLO, HBNB on flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+web app must be listening on 0.0.0.0:5000
+/: will display "Hello HBNB!"
+must use sctrict_slashes=False in route
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """displays 'Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
